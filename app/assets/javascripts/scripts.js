34b0f05f9b5e78e4fb6a1827dec13cee1018a1b0
ntuvera <ntuvera@gmail.com>
2014-07-15 17:14:41 -0400

    basic frontend JS validations for new entries

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,13 +1,17 @@
-function grabEntries(){
-  $.ajax({
-    url: "/users/2/entries",
-    dataType: "json",
-    success: function(data){
-      console.log(data)
+function validateForm() {
+  var x = document.forms["new-entry"]["entry[event]"].value;
+  if (x == null || x == "") {
+    alert("Event name must be filled out");
+    return false;
+  }
+  var y = document.forms["new-entry"]["entry[emotion]"].value;
+    if (y == null || y == "") {
+      alert("Choose an Emotion from the wheel");
+      return false;
     }
-  })
+  var z = document.forms["new-entry"]["entry[description]"].value;
+    if ( z == null ||  z == "") {
+      alert("Please add a small description");
+      return false;
+    }
 }
-
-
-// setTimeout(function(){
-//   $('#emotion-wheel').fadeIn('slow')}, 5000);
