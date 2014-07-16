a6158cad8863e09c03ea8f71e8379e6e17106289
ntuvera <ntuvera@gmail.com>
2014-07-16 11:42:09 -0400

    style login in, redirect after login to home

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -15,7 +15,7 @@ class UserSessionsController < ApplicationController
 
   def create
     if @user = login(params[:email], params[:password])
-      redirect_back_or_to user_entries_path(@user.id), notice: 'Login successful'
+      redirect_back_or_to user_path(@user.id), notice: 'Login successful'
     else
       flash.now[:alert] = 'Login failed'
       render action: 'new'
